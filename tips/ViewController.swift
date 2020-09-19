901cd429757b543caf802aa6dc946bdb76be3993
Rashon <rashonpoole2323@gmail.com>
2020-09-19 14:52:28 -0400

    Initial Commit

---
+++ tips/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tips
+//
+//  Created by Rashon Poole on 9/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
